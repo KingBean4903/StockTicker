c1f3347a342d9176a697c9fbf5cc73cbcc0a4b69
KingBean4903 <davidkanyango065@gmail.com>
2025-06-12 15:33:27 +0300

    Initial version v1.24.3

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/KingBean4903/StockTicker
+
+go 1.24.3
+
+require (
+	google.golang.org/grpc v1.73.0
+	google.golang.org/protobuf v1.36.6
+)
---
+++ server/main.go
@@ -0,0 +1,81 @@
+package main
+
+type stockTickerServer struct {
+	stock.UnimplementedStockTickerServer
+}
+
+import (
+	"context"
+	"log"
+	"math/rand"
+	"net"
+	"sync"
+	"time"
+	"google.golang.org/grpc"
+	"google.golang.org/credentials/secure"
+	stock "github.com/KingBean4903/StockTicker/stock"
+)
+
+type stockTickerServer struct {
+	stock.UnimplementedStockTickerServer  
+}
+
+
+func (s *stockTickerServer) Subscriber(req *stock.StockRequest, stream stock.StockTicker_SubscribeServer) error {
+
+	for  {
+		
+		for _, symbol := range req.Symbols {
+				
+			resp := &stock.StockResponse{
+					Symbol: symbol,
+					Price: generateFakePrice(symbol),
+					Timestamp: time.Now().Unix(),
+			}
+
+			if err := stream.Send(resp); err != nil {
+					log.Printf("Client disconnected: %v", err)
+					return err
+			}	
+		}
+
+		time.Sleep(1 * time.Second)
+
+	}
+
+}
+
+func generateFakePrice(symbol string) float64 {
+		basePrices := map[string]float64{
+					"AAPL" : 170.0,
+					"GOOG" : 148.0,
+					"MSFT" : 410.0,
+		}
+
+		base := basePrices[symbol]
+		fluctuation := rand.Float64() * 2.0 - 1.0
+		return base + fluctuation
+}
+
+
+
+func main() {
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	stock.RegisterStockTickerServer(s, &stockTickerServer())
+
+	log.printf("Server started at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+			log.Fatalf("Failed to start server: %v", err)
+	}
+
+}
+
+
+
+
